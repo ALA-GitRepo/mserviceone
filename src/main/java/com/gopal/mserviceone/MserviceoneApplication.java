23054246b0fcdf043d9ae31a3968743c07cda654
ALA-GitRepo <gopal.nair@accesslifeamerica.org>
2021-01-24 19:53:21 -0500

    Initial Commit for micro service one

---
+++ src/main/java/com/gopal/mserviceone/MserviceoneApplication.java
@@ -0,0 +1,28 @@
+package com.gopal.mserviceone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@EnableEurekaClient
+public class MserviceoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MserviceoneApplication.class, args);
+	}
+
+	@Value("${configmessage:default}")
+	private String message;
+
+	@GetMapping("/message")
+	private String getInfo() {
+		System.out.println("Message : " + message);
+		return message;
+	}
+
+}
---
+++ src/test/java/com/gopal/mserviceone/MserviceoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopal.mserviceone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MserviceoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
